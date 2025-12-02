13ffbb9d3e42df2c869d7142bfbcd22481d42f46
Estephanie Standen <esteph.csa@gmail.com>
2025-12-02 16:45:56 +0000

    Add minimal app.py template

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # TODO: handle password more securely
+    return render_template("index.html")
+
+@app.route("home")
+def index():
+    return render_template("home.html")
+
+@app.route("/rsvp")
+def rsvp():
+    return render_template("rsvp.html")
+
+@app.route("/travel")
+def travel():
+    return render_template("travel.html")
+
+@app.route("/airbnb")
+def schedule():
+    return render_template("airbnb.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
